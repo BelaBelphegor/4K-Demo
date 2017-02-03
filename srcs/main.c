4fd13a47e597d2562fb06d23c18012fe0d790edc
BelaBelphegor <tiboitel@student.42.fr>
2017-02-03 20:47:54 +0100

    Addition direction vector and velocity for metaballs.

--- srcs/main.c
+++ srcs/main.c
@@ -5,6 +5,9 @@
 #include <time.h>
 #include "metaball.h"
 
+#define WINDW_H 600
+#define WINDW_W 800
+
 void	SDL_PutPixel(SDL_Texture *texture, int x, int y, int color)
 {
 	int					pitch;
@@ -13,7 +16,7 @@ void	SDL_PutPixel(SDL_Texture *texture, int x, int y, int color)
 	(void)color;
 	(void)x;
 	(void)y;	
-	
+
 	SDL_Color	ucolor;
 	ucolor.r = 0;
 	ucolor.b = 80;
@@ -22,7 +25,7 @@ void	SDL_PutPixel(SDL_Texture *texture, int x, int y, int color)
 	{
 		dprintf(2, "Error: %s\n", SDL_GetError());
 		return ;
-	}
+	}	
 	position = y * (pitch / sizeof(unsigned int)) + x;
 	pixels[position] = color;
 	SDL_UnlockTexture(texture);
@@ -40,11 +43,11 @@ int		main(void)
 	int				quit;
 
 	/*
-	  ** Rendering variables
-	  */
-	unsigned int	pixels[800 * 600];
+	 ** Rendering variables
+	 */
+	unsigned int	pixels[WINDW_W * WINDW_H];
 	SDL_Texture 	*texture;
-  	int				i;
+	int				i;
 	int				j;
 
 	/*
@@ -66,26 +69,33 @@ int		main(void)
 	if (SDL_Init(SDL_INIT_VIDEO) != 0)
 		return (-1);
 	pwindow = SDL_CreateWindow("4K-demo", SDL_WINDOWPOS_CENTERED,
-			SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+			SDL_WINDOWPOS_CENTERED, WINDW_W, WINDW_H, SDL_WINDOW_SHOWN);
 	renderer = SDL_CreateRenderer(pwindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if (renderer == NULL)
 		return (-1);
-	 texture = SDL_CreateTexture(renderer,
-			SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, 640, 480);
-
+	texture = SDL_CreateTexture(renderer,
+			SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, WINDW_W, WINDW_H);
 	keystate = NULL;
 	keystate = (Uint8 *)SDL_GetKeyboardState(NULL);
 	quit = 1;
-//	memset(pixels, 255, 800 * 600 * sizeof(unsigned int));	
-//	SDL_UpdateTexture(texture, NULL, pixels, 800 * sizeof(unsigned int));
 	metaball_iterator = -1;
 	while (++metaball_iterator < MAX_METABALLS)
 	{
 		ball_list[metaball_iterator] = (t_metaball *)malloc(sizeof(t_metaball) + 1);
-		metaball_init(ball_list[metaball_iterator], rand() % 800, rand() % 600, 1 + rand() % 40);
+		metaball_init(ball_list[metaball_iterator], rand() % WINDW_W, rand() % WINDW_H, 1 + rand() % 40);
 	}
-	while (j = -1, ++i < 800)
-			while (++j < 600)
+	int		dirx;
+	int		diry;
+
+	dirx = -1;
+	diry =  1;
+	while (quit)
+	{
+		i = -1;
+		memset(pixels, 0, WINDW_W * WINDW_H * sizeof(unsigned int));	
+		SDL_UpdateTexture(texture, NULL, pixels, WINDW_W * sizeof(unsigned int));
+		while (j = -1, ++i < WINDW_H)
+			while (++j < WINDW_W)
 			{
 				sum = 0;
 				metaball_iterator = -1;
@@ -93,12 +103,19 @@ int		main(void)
 				{
 					sum += metaball_equation(ball_list[metaball_iterator], j, i);
 					if (sum >= MIN_THRESHOLD && sum <= MAX_THRESHOLD)
-						SDL_PutPixel(texture, i, j, 0x00FFFF00);
-
+						SDL_PutPixel(texture, j, i, rand() % 16777215);
 				}
 			}
-	while (quit)
-	{
+		metaball_iterator = -1;
+		while (++metaball_iterator < MAX_METABALLS)
+		{
+			if ((ball_list[metaball_iterator]->_x + ball_list[metaball_iterator]->_radius / 2) >= WINDW_W || ball_list[metaball_iterator]->_x <= 0 + ball_list[metaball_iterator]->_radius /2)
+				dirx *= -1;
+			if (ball_list[metaball_iterator]->_y + ball_list[metaball_iterator]->_radius / 2 >= WINDW_H || ball_list[metaball_iterator]->_y <= 0 + ball_list[metaball_iterator]->_radius /2)
+				diry *= -1;
+			ball_list[metaball_iterator]->_x += dirx * 3;
+			ball_list[metaball_iterator]->_y += diry * 3;
+		}
 		while (SDL_PollEvent(&e))
 		{
 			if (e.type == SDL_QUIT)
